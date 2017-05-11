c4c4f9991c75a6f06e0a3049a5d2cdc83a8d06e9
Joshua Woodcock <joshuawoodcock91@gmail.com>
2017-05-11 09:50:01 +0100

    got rid of errors

--- Assets/Scripts/Beginning OOP/FavouriteMovies.cs
+++ Assets/Scripts/Beginning OOP/FavouriteMovies.cs
@@ -2,12 +2,40 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour {
+struct MyFavouriteMovies
+{
+    public string GenreType;
+    public int StarRating;
+    public string[] MovieNames;           
+}
+
+public class FavouriteMovies : MonoBehaviour {
+
+    public string genreType;
+    public int starRating;
+    public string[] movies; 
 
 	// Use this for initialization
 	void Start () {
 		
 	}
+
+    void OnDisable()
+    {
+        MyFavouriteMovies myFavouriteMovies = new MyFavouriteMovies();
+        myFavouriteMovies.GenreType = genreType;
+        myFavouriteMovies.StarRating = starRating;
+        myFavouriteMovies.MovieNames = movies;
+
+        Debug.Log("genre type: " + myFavouriteMovies.GenreType);
+        Debug.Log("star rating: " + myFavouriteMovies.StarRating);
+        Debug.Log("movie names: ");
+        foreach (string movieName in myFavouriteMovies.MovieNames)
+        {
+            Debug.Log(movieName);
+        }
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
