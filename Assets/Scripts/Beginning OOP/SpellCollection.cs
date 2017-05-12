6532a830c5105fcd7865999289cffec9caf4fa6f
Joshua Woodcock <joshuawoodcock91@gmail.com>
2017-05-12 16:32:28 +0100

    completed Constructors challenge

---
+++ Assets/Scripts/Beginning OOP/SpellCollection.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpellCollection : MonoBehaviour {
+
+    void OnDisable()
+    {
+        Spell IceBall = new Spell("Iceball", 10, 1);
+        Spell Fireball = new Spell("Fireball", 100, 1);
+        Spell EarthShake = new Spell("EarthShake", 10, 100.0f);
+
+        Debug.Log(IceBall.GetSpellStats());
+        Debug.Log(Fireball.GetSpellStats());
+        Debug.Log(EarthShake.GetSpellStats());
+
+    }
+}
---
+++ Assets/Scripts/Beginning OOP/Spells.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct Spell
+{
+    string Name { get; set; }
+    int Damage { get; set; }
+    float DamageEffect { get; set; }
+
+    public Spell(string name, int damage, float damageEffect)
+    {
+        this.Name = name;
+        this.Damage = damage;
+        this.DamageEffect = damageEffect;
+    }
+
+    public string GetSpellStats()
+    {
+        string spellStats = " ";
+        spellStats += "Name: " + this.Name + " ";
+        spellStats += "Damage: " + this.Damage + " ";
+        spellStats += "Damage Effect: " + this.DamageEffect + " ";
+        return spellStats;
+
+    }
+
+}
