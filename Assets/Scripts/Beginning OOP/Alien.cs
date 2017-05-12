a91734b34f36f7a6906afd665fdf5102162cdfe5
Joshua Woodcock <joshuawoodcock91@gmail.com>
2017-05-12 10:42:59 +0100

    initial commit

---
+++ Assets/Scripts/Beginning OOP/Alien.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace RayWenderlich.BeginingCSharp.Demonstartion
+{
+    struct Alien
+    {
+        public int HitPoints;
+    }
+}
+
+
+
+public class Alien : MonoBehaviour {
+
+    public Text playerName;
+
+
+
+	// Use this for initialization
+	void Start () {
+        RayWenderlich.BeginingCSharp.Demonstartion.Alien alien = new RayWenderlich.BeginingCSharp.Demonstartion.Alien ();
+        alien.HitPoints = 100; 
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/Beginning OOP/PlayerNames.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RayWenderlich.BeginingCSharp.Demonstartion
+{
+    struct PlayerNames
+    {
+        public string FirstName;
+        public string LastName;
+    }
+}
+
+namespace RayWenderlich.BeginingCSharp.Final
+{
+    struct InitScores
+    {
+        public string Initials;
+        public int Score;
+    }
+}
+
+public class PlayerNames : MonoBehaviour {
+
+    public string firstName;
+    public string lastName;
+    public string initials;
+    public int score;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnDisable ()
+    {
+        RayWenderlich.BeginingCSharp.Demonstartion.PlayerNames playerName = new RayWenderlich.BeginingCSharp.Demonstartion.PlayerNames();
+        playerName.FirstName = firstName;
+        playerName.LastName = lastName;
+
+        Debug.Log("Your name is " + playerName.FirstName + " " + playerName.LastName);
+
+        RayWenderlich.BeginingCSharp.Final.InitScores initScores = new RayWenderlich.BeginingCSharp.Final.InitScores();
+        initScores.Initials = initials;
+        initScores.Score = score;
+
+        Debug.Log("Your initials are " + initScores.Initials + " " + "Your score is "+ initScores.Score);
+
+
+
+
+    }
+
+}
