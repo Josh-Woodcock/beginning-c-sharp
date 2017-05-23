93a555768533fd9c44473e85db0f2c64fe2c59d4
Joshua Woodcock <joshuawoodcock91@gmail.com>
2017-05-23 16:28:59 +0100

    completed demo and challenge for "Overloading Classes"

---
+++ Assets/Scripts/Intermediate OOP/GoKart.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+class GK
+{
+    private int speed;
+
+    public void GetSpeed(int speed)
+    {
+        this.speed += speed;
+        Debug.Log("The current speed is " + this.speed);
+    }
+
+    public void GetSpeed(int speed, bool turbo)
+    {
+        if (turbo == true)
+        {
+            GetSpeed(speed * 2);
+        }
+        else
+        {
+            GetSpeed(speed);
+        }
+    }
+}
+
+
+public class GoKart : MonoBehaviour {
+
+    void OnDisable()
+    {
+        GK marioKart = new GK();
+        marioKart.GetSpeed(10);
+        marioKart.GetSpeed(5, true);
+    }
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/Intermediate OOP/PlayerCharacter.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+class PC
+{
+    private string name = " ";
+    private string twitter = " ";
+    private string forumName = " ";
+
+    public void SetPlayerInfo(string name)
+    {
+        this.name = name;
+        Debug.Log("called with one string");
+    }
+
+    public void SetPlayerInfo(string name, string twitterHandle)
+    {
+        this.twitter = twitterHandle;
+        Debug.Log("called with two strings");
+        SetPlayerInfo(name);
+    }
+
+    public void SetPlayerInfo(string name, string twitter, string forumName)
+    {
+        this.forumName = forumName;
+        Debug.Log("called with three strings");
+        SetPlayerInfo(name, twitter);
+    }
+}
+
+public class PlayerCharacter : MonoBehaviour {
+
+    void OnDisable()
+    {
+        PC playerCharacter = new PC();
+        playerCharacter.SetPlayerInfo("Josh","@JoshWoodcock", "hellisinhello");
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
