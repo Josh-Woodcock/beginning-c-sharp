279b2cde2b9ca96debaf9534596275a7e64692a5
Joshua Woodcock <joshuawoodcock91@gmail.com>
2017-05-23 15:48:33 +0100

    "Abstarct Classes" challenge complete

---
+++ Assets/Scripts/Intermediate OOP/Vehicles.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+abstract class Vehicle
+{
+    public abstract string IncreaseSpeed();
+}
+
+class Car : Vehicle
+{
+    public override string IncreaseSpeed()
+    {
+        return "VROOM";
+    }
+}
+
+
+class Tank : Vehicle
+{
+    public override string IncreaseSpeed()
+    {
+        return "RUMBLE";
+    }
+}
+
+public class Vehicles : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnDisable()
+    {
+        Vehicle[] vehicles = new Vehicle[] { new Car(), new Tank()};
+
+        foreach (Vehicle vehicle in vehicles)
+        {
+            Debug.Log(vehicle.IncreaseSpeed());
+        }
+    }
+}
