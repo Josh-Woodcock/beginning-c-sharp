cac7edfea04d765ecc1f3a9c381ddedee91dd0c1
Joshua Woodcock <joshuawoodcock91@gmail.com>
2017-05-23 14:18:53 +0100

    completed demo stage of "Overriding"

---
+++ Assets/Scripts/Intermediate OOP/Enemy.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy {
+
+    protected int hitPoints;
+    public enum DamageType { Water, Fire };
+
+    public virtual void TakeDamage(int amount, DamageType damageType)
+    {
+        hitPoints -= amount;
+    }
+
+    public string GetHitPoints()
+    {
+        return " " + hitPoints;
+    }
+}
+
+class WaterEnemy : Enemy
+{
+    public WaterEnemy()
+    {
+        hitPoints = 30;
+    }
+
+
+    public override void TakeDamage(int amount, DamageType damageType)
+    {
+       if (damageType == DamageType.Fire)
+        {
+            base.TakeDamage(amount * 2, damageType);
+        }
+       else
+        {
+            base.TakeDamage(amount / 2, damageType);
+        }
+    }
+}
+
+class FireEnemy : Enemy
+{
+    public override void TakeDamage(int amount, DamageType damageType)
+    {
+        if (damageType == DamageType.Fire)
+        {
+            base.TakeDamage(amount * 10, DamageType.Water);
+        }
+        else
+        {
+            base.TakeDamage(amount / 2, damageType);
+        }
+    }
+}
---
+++ Assets/Scripts/Intermediate OOP/EnemyDemo.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDemo : MonoBehaviour {
+
+	void OnDisable()
+    {
+        WaterEnemy waterEnemy = new WaterEnemy();
+        waterEnemy.TakeDamage(10, Enemy.DamageType.Fire);
+        Debug.Log(waterEnemy.GetHitPoints());
+    }
+    
+    
+    
+    // Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
