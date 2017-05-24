8f6618e8f8abc640c75cdaa6be1a719f426d5b5c
Joshua Woodcock <joshuawoodcock91@gmail.com>
2017-05-24 12:20:22 +0100

    completed "Interfaces" demo and challenge

---
+++ Assets/Scripts/Intermediate OOP/InterfaceDemo.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+interface IShootable
+{
+    void Fire();
+    void SayHello();
+}
+
+class Jet : IShootable
+{
+    public void Fire()
+    {
+        Debug.Log("Jet Fires");
+    }
+
+    public void SayHello()
+    {
+        Debug.Log("hi");
+    }
+}
+
+class InterfaceTank : IShootable
+{
+    public void Fire()
+    {
+        Debug.Log("Tank Fires");
+    }
+
+    public void SayHello()
+    {
+        Debug.Log("hi");
+    }
+}
+
+class Turret : IShootable
+{
+    public void Fire()
+    {
+        Debug.Log("Turret Fires");
+    }
+
+    public void SayHello()
+    {
+        Debug.Log("hi");
+    }
+}
+
+public class InterfaceDemo : MonoBehaviour {
+
+    void OnDisable()
+    {
+        Jet jet = new Jet();
+        InterfaceTank tank = new InterfaceTank();
+        Turret turret = new Turret();
+
+        IShootable[] shootables = new IShootable[] { jet, tank, turret };
+
+        foreach ( IShootable shootable in shootables)
+        {
+            shootable.Fire();
+            shootable.SayHello();
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/Intermediate OOP/MovableObjects.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+interface IMovable
+{
+    void Drive();
+}
+
+class Bike : IMovable
+{
+    public void Drive()
+    {
+        Debug.Log("crank crank");
+    }
+}
+
+class InterfaceCar : IMovable
+{
+    public void Drive()
+    {
+        Debug.Log("vroom vroom");
+    }
+}
+
+class GolfCart : IMovable
+{
+   public void Drive()
+    {
+        Debug.Log("silent");
+    }
+}
+
+
+
+
+public class MovableObjects : MonoBehaviour {
+
+    void OnDisable()
+    {
+        IMovable[] movables = new IMovable[] { new Bike() , new InterfaceCar(), new GolfCart()};
+
+        foreach (IMovable movable in movables )
+        {
+            movable.Drive();
+        }
+    }
+
+
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
