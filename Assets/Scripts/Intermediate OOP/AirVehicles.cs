e3ee6e9203ea7b8610390322a48e4bc0d772843f
Joshua Woodcock <joshuawoodcock91@gmail.com>
2017-05-24 09:58:50 +0100

    completed class constructors demo and challenge

---
+++ Assets/Scripts/Intermediate OOP/AirVehicles.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plane
+{
+    protected string name { set; get; }
+    protected int passengerNumber { set; get; }
+
+    public Plane(string name, int passengerNumber)
+    {
+        this.name = name;
+        this.passengerNumber = passengerNumber;
+        Debug.Log("Plane constructor");
+    }
+}
+
+
+
+public class HangGlider : Plane
+{
+    protected float pilotWeight { get; set; }
+
+    public HangGlider(string name, int passengerNumber, float pilotWeight) : base(name, passengerNumber)
+    {
+        this.pilotWeight = pilotWeight;
+        Debug.Log("HangGlider Constructor");
+    }
+}
+
+
+public class AirVehicles : MonoBehaviour
+{
+
+
+    void OnDisable()
+    {
+        HangGlider hangGlider = new HangGlider("Bob", 1, 92.5f);
+
+        foreach (HangGlider hangGlider in y) {
+
+        } 
+    }
+
+}
+
+
+
---
+++ Assets/Scripts/Intermediate OOP/Constructors.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Transport
+{
+    protected string name { set; get; }
+    public Transport(string name)
+    {
+        this.name = name;
+        Debug.Log("Transport constructor");
+    }
+}
+
+public class Boat : Transport
+{
+    protected double speed { get; set; }
+
+    public Boat(string name, double speed) : base(name)
+    {
+        this.speed = speed;
+        Debug.Log("Boat constructor");
+    }
+}
+
+public class JetSki : Boat
+{
+    protected int numberOfPassengers { get; set; }
+
+    public JetSki(string name, double speed, int numberOfPassengers) : base(name, speed)
+    {
+        this.numberOfPassengers = numberOfPassengers;
+        Debug.Log("JetSki constructor");
+    }
+}
+
+
+
+public class Constructors : MonoBehaviour {
+
+    void OnDisable()
+    {
+        JetSki jetSki = new JetSki("C# Mobile", 100.0, 2);
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
